3cf8e83a9b73a6abb3c6548e4f7d70a3143eea32
Harmit <harmitpatel110@gmail.com>
2024-11-07 22:38:57 -0500

    Registering (Create account) functionalities are completed with firebase db

--- src/components/Modals/Signup.tsx
+++ src/components/Modals/Signup.tsx
@@ -1,19 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSetRecoilState } from 'recoil';
 import { authModalState } from '@/atoms/authModalAtom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { auth } from '@/firebase/firebase'
+import { useRouter } from 'next/router';
+
 type SignupProps = {
 
 };
 
 const Signup: React.FC<SignupProps> = () => {
     const setAuthModalState = useSetRecoilState(authModalState);
+    const router = useRouter();
+
     const handleClick = () => {
         setAuthModalState((prev) => ({ ...prev, type: 'login' }));
     };
 
 
+    const [inputs,setInputs] = useState({ email: '', displayName: '', password: '' });
+
+    const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    };
+
+   
+
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
+
+
+
+
+
+
+    const handleRegister = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if (!inputs.email || !inputs.password|| !inputs.displayName) return alert("Please fill all fields");
+        try {
+
+            const newUser = await createUserWithEmailAndPassword(inputs.email, inputs.password);
+            if (!newUser) return;
+            router.push('/');
+
+        } catch (error: any) {
+
+            alert(error.message);
+        }
+    };
+
+
+    useEffect(()=>{if(error) alert(error.message)},[error]);
+
     return (
-        <form className='space-y-6 px-6 pb-4'>
+        <form className='space-y-6 px-6 pb-4' onSubmit={handleRegister}>
             <h3 className="text-xl font-medium text-white">Register to LeetClone</h3>
 
             {/* For Email */}
@@ -21,7 +66,7 @@ const Signup: React.FC<SignupProps> = () => {
                 <label htmlFor="email" className="text-sm font-bold block mb-2 text-gray-200">
                     Email Address
                 </label>
-                <input type="email" name="email" id="email" className='border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full
+                <input onChange={handleChangeInput} type="email" name="email" id="email" className='border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full
         p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white'
 
                     placeholder='name@example.com'
@@ -34,7 +79,7 @@ const Signup: React.FC<SignupProps> = () => {
                 <label htmlFor="displayName" className="text-sm font-bold block mb-2 text-gray-200">
                     Display Name
                 </label>
-                <input type="displayName" name="displayName" id="displayName" className='border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full
+                <input onChange={handleChangeInput} type="displayName" name="displayName" id="displayName" className='border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full
         p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white'
 
                     placeholder='Jhon Smith'
@@ -47,7 +92,7 @@ const Signup: React.FC<SignupProps> = () => {
                 <label htmlFor="password" className="text-sm font-bold block mb-2 text-gray-200">
                     Password
                 </label>
-                <input type="password" name="password" id="password" className='border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full
+                <input onChange={handleChangeInput} type="password" name="password" id="password" className='border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full
         p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white'
 
                     placeholder='*********'
@@ -57,7 +102,7 @@ const Signup: React.FC<SignupProps> = () => {
 
             {/* For Register button*/}
             <button type="submit" className='w-full text-white focus:ring-blue-300 font-bold rounded-lg text-sm px-5 py-2.5 text-center bg-brand-orange hover:bg-brand-orange-s'>
-                Register
+                {loading ?"Registering..." : "Register"} 
             </button>
 
 
