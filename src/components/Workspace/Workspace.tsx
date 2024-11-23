9fbfc5f7b9bab5066b18f241b424f98e83a9ffa7
Harmit <harmitpatel110@gmail.com>
2024-11-22 20:49:31 -0500

    Workspace Playground UI completed with problem,code editor and submission split section

--- src/components/Workspace/Workspace.tsx
+++ src/components/Workspace/Workspace.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Split from 'react-split'
 import ProblemDescription from './ProblemDescription/ProblemDescription';
+import Playground from './Playground/Playground';
 
 type WorkspaceProps = {
 
@@ -10,7 +11,8 @@ const Workspace:React.FC<WorkspaceProps> = () => {
     
     return <Split className="split" minSize={0}>
         <ProblemDescription />
-        <div>Code editor will be here.</div>
+        <Playground />
+        
     </Split>;
 };
 export default Workspace;
